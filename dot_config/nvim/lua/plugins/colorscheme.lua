33b78b588d0226d3ee2262e88be04614cbc21997
Ivan Klymenchenko <hello@iillexial.me>
2024-04-22 16:17:13 +0200

    fix theme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -4,11 +4,12 @@
 --})
 --
 --vim.cmd.colorscheme("gruvbox")
-vim.o.termguicolors = true
-vim.cmd.colorscheme "catppuccin"
 require("catppuccin").setup({
     flavour = "macchiato", -- latte, frappe, macchiato, mocha
     dim_inactive = {
 	enabled = false,
     },
 })
+
+vim.o.termguicolors = true
+vim.cmd.colorscheme "catppuccin"
